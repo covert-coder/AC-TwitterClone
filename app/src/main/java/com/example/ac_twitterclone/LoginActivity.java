39d9e59796af52a10623fb195e5763500dc91bca
covert_coder <rgeeinstead@gmail.com>
2020-04-10 10:26:51 -0600

    AC-Twitter clone, early setup, April 10, 2020, creating layout for login and signup with transitions

---
+++ app/src/main/java/com/example/ac_twitterclone/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.ac_twitterclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        setTitle("Login to Twitter");
+    }
+}
